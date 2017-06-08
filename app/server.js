a0e856aebf5cac30956e7a726bb4257a8507209b
annie ke <annieke8@gmail.com>
2017-06-08 15:34:23 -0700

    simple response with name

--- app/server.js
+++ app/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
+import dotenv from 'dotenv';
+import botkit from 'botkit';
+
+dotenv.config({ silent: true });
 
 // initialize
 const app = express();
@@ -31,3 +35,36 @@ const port = process.env.PORT || 9090;
 app.listen(port);
 
 console.log(`listening on: ${port}`);
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM((err) => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
+
+controller.hears(
+  ['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'],
+  (bot, message) => {
+    bot.api.users.info({ user: message.user }, (err, res) => {
+      if (res) {
+        bot.reply(message, `Hello, ${res.user.name}!`);
+      } else {
+        bot.reply(message, 'Hello there!');
+      }
+    });
+  },
+);
